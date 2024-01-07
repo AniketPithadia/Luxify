617152091648a28d748f81a325b2d91a8b050ce4
AniketPithadia <aniketpithadia@gmail.com>
2024-01-06 21:19:18 -0500

    updated files for deployment

--- backend/index.js
+++ backend/index.js
@@ -10,6 +10,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
+
+// FOR DEPLOYMENT
+
+const __dirname = path.resolve();
+
+// FOR DEPLOYMENT
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   connectToMongoDB();
@@ -21,6 +27,10 @@ app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
 app.use("/api/listing", listingRoutes);
 
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.use("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || "Internal Server Error";
